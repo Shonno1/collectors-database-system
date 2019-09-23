fd1c4833744ac07979a686011424eeda20364b65
Shonno1 <seandouglaslilley@googlemail.com>
2019-09-23 15:58:57 +0100

    Database Connection function added

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+function connectDb () {
+    // connect to database
+    $db = new PDO ('mysql:host=db; dbname=seanCollection','root','password');
+    // set default return mode to ASSOC_ARRAY
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    // return the connection variable
+    return $db;
+}
